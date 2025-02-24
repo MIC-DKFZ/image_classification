e44a9823b3f7cecc0cfbf317295ebf34c3ec077b
d246a <d.trofimova@dkfz-heidelberg.de>
2025-02-24 14:11:40 +0100

    debian

--- load_env_debian.sh
+++ load_env_debian.sh
@@ -11,7 +11,8 @@ export CPATH=/usr/local/lib:$CPATH
 export PATH=/usr/local/cuda-${CUDA}/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-${CUDA}/lib64:$LD_LIBRARY_PATH
 export CUDA_HOME=/usr/local/cuda-${CUDA}
-
+module load GCC/14.1.0
+module load binutils/2.42
 . ~/.bashrc
 conda activate ${CONDA_ENV}
 
@@ -19,4 +20,4 @@ export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=1
 
 export DATASET_LOCATION=/dkfz/cluster/gpu/data/OE0441/s522r/ILSVRC_2012
-export EXPERIMENT_LOCATION=/dkfz/cluster/gpu/checkpoints/OE0441/d246a
+export EXPERIMENT_LOCATION=/dkfz/cluste r/gpu/checkpoints/OE0441/d246a
